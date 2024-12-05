dea870289ac242aba0a8dc9cdf8ff6ffd6c1a879
neju050405 <75866073+neju050405@users.noreply.github.com>
2024-12-05 16:43:12 -0300

    Update nav.js

--- src/nav.js
+++ src/nav.js
@@ -1,21 +1,27 @@
 import React from "react";
-import logoMark from "./img/logoMark.png";
 import './nav.css';
+import logoMark from "./img/logoMark.png";
 
 const Nav = () => {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a className="navbar-brand" href="#"><img src={logoMark} className="logoMark" /></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand" href="#"><img src={logoMark}/></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a className="nav-link" href="#">Sobre Nós</a>
-                        <a className="nav-link" href="#brands">Marcas</a>
-                        <a className="nav-link" href="#endereços">Endereços</a>
-                    </div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a className="nav-link" href="#">Sobre Nós</a>
+                        </li>
+                        <li class="nav-item">
+                            <a className="nav-link" href="#brands">Marcas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a className="nav-link" href="#endereços">Endereços</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
